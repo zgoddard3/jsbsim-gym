Quaternion fixes

jsbsim_gym.py
@@ -25,8 +25,8 @@ class JSBSimEnv(gym.Env):
     def __init__(self, root='.'):
         self.simulation = jsbsim.FGFDMExec(root, None)
         self.simulation.set_debug_level(0)
-        self._set_initial_conditions()
         self.simulation.load_model('f16')
+        self._set_initial_conditions()
         self.simulation.run_ic()
         self.down_sample = 4
         self.state = np.zeros(12)
@@ -35,7 +35,7 @@ class JSBSimEnv(gym.Env):
 
     def _set_initial_conditions(self):
         self.simulation.set_property_value('propulsion/set-running', -1)
-        self.simulation.set_property_value('ic/u-fps', 900)
+        self.simulation.set_property_value('ic/u-fps', 900.)
         self.simulation.set_property_value('ic/h-sl-ft', 5000)
     
     def step(self, action):
@@ -47,6 +47,8 @@ class JSBSimEnv(gym.Env):
         self.simulation.set_property_value("fcs/throttle-cmd-norm", throttle)
 
         for _ in range(self.down_sample):
+            self.simulation.set_property_value("propulsion/tank/contents-lbs", 1000)
+            self.simulation.set_property_value("propulsion/tank[1]/contents-lbs", 1000)
             self.simulation.run()
 
         self._get_state()
@@ -68,6 +70,9 @@ class JSBSimEnv(gym.Env):
     
     def reset(self, seed=None):
         self.simulation.run_ic()
+        self.simulation.set_property_value('propulsion/set-running', -1)
+        self.simulation.set_property_value("gear/gear-cmd-norm", 0.0)
+        self.simulation.set_property_value("gear/gear-pos-norm", 0.0)
 
         rng = np.random.default_rng(seed)
         distance = rng.random() * .1 + .01
@@ -76,22 +81,40 @@ class JSBSimEnv(gym.Env):
 
         self.goal[:2] = np.cos(bearing), np.sin(bearing)
         self.goal[:2] *= distance
-        self.goal[3] = altitude
+        self.goal[2] = altitude
+
+        self._get_state()
+
+        return self.state.copy()
     
     def render(self):
+        scale = 1e-4
+
         if self.viewer is None:
             self.viewer = Viewer(1280, 720)
 
             f16_mesh = load_mesh(self.viewer.ctx, self.viewer.prog, "f16.obj")
             self.f16 = RenderObject(f16_mesh)
+            self.f16.transform.scale = 1/30
+            self.f16.color = 0, 0, .6
             self.viewer.objects.append(self.f16)
-            self.viewer.objects.append(Grid(self.viewer.ctx, self.viewer.prog, 21, 1.))
+            self.viewer.objects.append(Grid(self.viewer.ctx, self.viewer.unlit, 21, 1.))
         
-        self.f16.transform.x = self.state[0] * 100
-        self.f16.transform.y = self.state[1] * 100
-        self.f16.transform.z = self.state[2] / 10
+        # Rough conversion from lat/long to meters
+        x, y = self.state[:2] * 4e7
 
-        self.f16.transform.rotation = Quaternion.from_euler(*self.state[9:])
+        self.f16.transform.z = x * scale
+        self.f16.transform.x = -y * scale
+        self.f16.transform.y = self.state[2] * scale
+
+        # print(self.f16.transform.position)
+
+        # rot = Quaternion.from_euler(-self.state[10], -self.state[11], self.state[9], mode=1)
+        rot = Quaternion.from_euler(*self.state[9:])
+        rot = Quaternion(rot.w, -rot.y, -rot.z, rot.x)
+        self.f16.transform.rotation = rot
+
+        self.viewer.set_view(-y * scale, self.state[2] * scale + 1, x * scale - 3, Quaternion.from_euler(np.pi/12, 0, 0, mode=1))
 
         self.viewer.render()
     
@@ -99,3 +122,14 @@ class JSBSimEnv(gym.Env):
         if self.viewer is not None:
             self.viewer.close()
             self.viewer = None
+
+if __name__ == "__main__":
+    from time import sleep
+    env = JSBSimEnv()
+    env.reset()
+    env.render()
+    for _ in range(300):
+        env.step(np.array([0.05, -0.2, 0, .5]))
+        env.render()
+        sleep(1/30)
+    env.close()

quaternion.py
@@ -35,11 +35,14 @@ class Quaternion:
                         [2*(x*z-y*w), 2*(y*z+x*w), 1-2*(x**2+y**2)]])
     
     @classmethod
-    def from_euler(cls, phi, theta, psi):
+    def from_euler(cls, phi, theta, psi, mode=0):
         q1 = Quaternion(np.cos(phi/2), np.sin(phi/2), 0, 0)
         q2 = Quaternion(np.cos(theta/2), 0, np.sin(theta/2), 0)
         q3 = Quaternion(np.cos(psi/2), 0, 0, np.sin(psi/2))
-        return q1*q2*q3
+        if mode == 0:
+            return q3*q2*q1
+        else:
+            return q2*q1*q3
     
     def __repr__(self):
         return f"<Quaternion: w={self.w}, x={self.x}, y={self.y}, z={self.z}>"