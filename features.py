94074bfec4dc21eb3cf70fc64712ff64f19934b6
Zachary Goddard <zgoddard3@gmail.com>
2022-12-19 19:45:42 -0500

    Added comments

--- features.py
+++ features.py
@@ -3,11 +3,38 @@ import torch as th
 from stable_baselines3.common.torch_layers import BaseFeaturesExtractor
 
 class JSBSimFeatureExtractor(BaseFeaturesExtractor):
+    """
+    Feature extractor to help learn the JSBSim environment. 
+    
+    ### Position
+    This extractor converts the position to relative cylindrical coordinates. Raw
+    altitude is also preserved since it's necessary to avoid crashing. 
+    
+    The distance to the goal is normalized as 1/(1+distance*scale). 'Scale' is a 
+    constant that we have set to 1e-3 (meters to kilometers). The rest of the
+    equation bounds the value between 0 and 1. Additionally it approaches 0 as
+    distance goes to infinity. This means the impact of distance on the network
+    diminishes as it increases. The intuition behind this is that the policy 
+    should depend more on relative bearing at greater distance (e.g. just turn to
+    face the goal and fly straight.)
+
+    Relative height to the goal and raw altitude are normalized by the estimated 
+    flight ceiling of the F-16 (15000 meters).
+
+    ### Velocities and angular rates
+    Velocities are left unchanged since mach, alpha, and beta are already pretty 
+    well scaled. Angular rates are also left unchanged since they are unlikely to
+    grow too large in practice due to the low-level regulator on the JSBSim model.
+
+    ### Angles
+    All angles (attitude, relative bearing, alpha, beta) are converted to sine-
+    cosine pairs. This makes sure that pi and -pi are the same in the feature 
+    space and will produce the same output.  
+    """
     def __init__(self, observation_space):
         super().__init__(observation_space, 17)
 
     def forward(self, observations):
-
         # Unpack
         position = observations[:,:3]
         mach = observations[:,3:4]
