04d2a3542a60a1cd29cc5e43acce2900c320ffcf
Zachary Goddard <zgoddard3@gmail.com>
2022-12-21 18:24:33 -0500

    Updated training iterations

--- train.py
+++ train.py
@@ -14,7 +14,7 @@ log_path = path.join(path.abspath(path.dirname(__file__)), 'logs')
 
 try:
     model = SAC('MlpPolicy', env, verbose=1, policy_kwargs=policy_kwargs, tensorboard_log=log_path, gradient_steps=-1, device='cuda')
-    model.learn(4000000)
+    model.learn(3000000)
 finally:
     model.save("models/jsbsim_sac")
     model.save_replay_buffer("models/jsbsim_sac_buffer")
